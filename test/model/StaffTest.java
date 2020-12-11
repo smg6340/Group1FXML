23d445a96137c1487f50b9b95583c711347dd16e
Sabrina Gooptu <smg6340@psu.edu>
2020-12-11 16:36:20 -0500

    Added simple JUnit Testing to test if id and/or lastname is null

---
+++ test/model/StaffTest.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author smg6340
+ */
+public class StaffTest {
+    
+    private String lastname;
+    private Integer id;
+    
+    public StaffTest() {
+    }
+
+    /**
+     * Test of getId method, of class Staff.
+     */
+    @Test
+    public void testGetId() {
+    }
+
+    /**
+     * Test of setId method, of class Staff.
+     */
+    @Test
+    public void testSetId() {
+        Assertions.assertNotNull(id);
+    }
+
+    /**
+     * Test of getCourse method, of class Staff.
+     */
+    @Test
+    public void testGetCourse() {
+    }
+
+    /**
+     * Test of setCourse method, of class Staff.
+     */
+    @Test
+    public void testSetCourse() {
+    }
+
+    /**
+     * Test of getLastname method, of class Staff.
+     */
+    @Test
+    public void testGetLastname() {
+    }
+    
+    /**
+     * Test of setLastname method, of class Staff.
+     */
+    @Test
+    public void testSetLastname() {
+        Assertions.assertNotNull(lastname);
+    }
+
+    /**
+     * Test of getAssignments method, of class Staff.
+     */
+    @Test
+    public void testGetAssignments() {
+    }
+
+    /**
+     * Test of setAssignments method, of class Staff.
+     */
+    @Test
+    public void testSetAssignments() {
+    }
+
+    /**
+     * Test of hashCode method, of class Staff.
+     */
+    @Test
+    public void testHashCode() {
+    }
+
+    /**
+     * Test of equals method, of class Staff.
+     */
+    @Test
+    public void testEquals() {
+    }
+
+    /**
+     * Test of toString method, of class Staff.
+     */
+    @Test
+    public void testToString() {
+    }
+
+    /**
+     * Test of getOfficehours method, of class Staff.
+     */
+    @Test
+    public void testGetOfficehours() {
+    }
+
+    /**
+     * Test of setOfficehours method, of class Staff.
+     */
+    @Test
+    public void testSetOfficehours() {
+    }
+    
+}
